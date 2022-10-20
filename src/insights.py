8b9909d9a7b0fe3e5d0562cafefb564e83ed5c85
thiago <thiagoedusan5.11@outlook.com>
2022-10-20 20:21:59 -0300

    feat: foi implementada no modulo src/insights.py uma função responsavel por retornar uma lista com todas as industria existentes no arquivo lido

--- src/insights.py
+++ src/insights.py
@@ -33,21 +33,14 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    industries = []
 
-    Must call `read`
+    with open(path) as file:
+        for index in csv.DictReader(file):
+            if index['industry'] not in industries:
+                industries.append(index['industry'])
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    return [industry for industry in industries if industry != '']
 
 
 def filter_by_industry(jobs, industry):
--- src/jobs.py
+++ src/jobs.py
@@ -4,8 +4,6 @@ from functools import lru_cache
 
 @lru_cache
 def read(path):
-    lines = []
-
     with open(path) as file:
         data = csv.DictReader(file)
         lines = [index for index in data]
