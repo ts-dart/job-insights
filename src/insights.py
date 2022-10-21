a1169264a6d2dfb7c7ec7b3e3be8f4bba55f0907
thiago <thiagoedusan5.11@outlook.com>
2022-10-21 11:49:12 -0300

    feat: foi implementada uma função que retorna uma lista com todos os empregos de uma determinada "industria"

--- src/insights.py
+++ src/insights.py
@@ -30,21 +30,7 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    return [job for job in jobs if industry == job['industry']]
 
 
 def get_max_salary(path):
