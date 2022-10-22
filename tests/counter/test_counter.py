7e656d1400035f43f3d491d7a4217d97049f73f1
thiago <thiagoedusan5.11@outlook.com>
2022-10-22 18:57:01 -0300

    test: foi feito teste unitario na função "count_ocurrences"

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,16 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    word_python = count_ocurrences('src/jobs.csv', 'Python')
+    word_javascript = count_ocurrences('src/jobs.csv', 'Javascript')
+
+    assert word_python == counter_fake_mock('src/jobs.csv', 'Python')
+    assert word_javascript == counter_fake_mock('src/jobs.csv', 'Javascript')
+
+
+def counter_fake_mock(path, word):
+    with open(path, mode="r") as file:
+        data = file.read()
+        count = data.lower().count(word.lower())
+        return count
